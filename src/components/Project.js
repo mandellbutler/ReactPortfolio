9d1b82015aeb37e58b390488801563474404d2a8
Mandell Butler <mandell.butler@outlook.com>
2021-06-15 17:33:27 -0400

    Created project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,9 +9,84 @@ import bridal from '../assets/images/bridal.jpeg';
 // Here we are importing a CSS file as a dependency
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const projects = [
+  {
+    title: 'horiseon',
+    href: 'https://franklincodes1.github.io/GroupProject1/',
+    image: {
+      src: horiseon,
+      alt: 'A man leading a team strategy meeting.'
+    }
+  },
+  {
+    title: 'On The Way',
+    href: 'https://franklincodes1.github.io/GroupProject1/',
+    image: {
+      src: otw,
+      alt: 'Image of gps mapping application'
+    }
+  },
+  {
+    title: 'Work Day Scehduler',
+    href: 'https://mandellbutler.github.io/Day-Planner/',
+    image: {
+      src: planner,
+      alt: 'Image of a work day scheduler with organized rows.'
+    }
+  },
+  {
+    title: 'Password Generator',
+    href: 'https://mandellbutler.github.io/Password-Genie/',
+    image: {
+      src: password,
+      alt: 'Image of a password generating application'
+    }
+  },
+  {
+    title: 'Movie Logger',
+    href: 'https://github.com/mandellbutler/projectTwo',
+    image: {
+      src: movie,
+      alt: 'Image of a movie logger application, displaying old movie posters.'
+    }
+  },
+  {
+    title: 'Devine Cupcakes',
+    href: 'https://github.com/mandellbutler',
+    image: {
+      src: cupcakes,
+      alt: 'Image of a gourmet cupcake display.'
+    }
+  },
+  {
+    title: "Kayla's Bridal",
+    href: 'https://github.com/mandellbutler',
+    image: {
+      src: bridal,
+      alt: 'Image of a gorgeous wedding dress draped in the sun.'
+    }
+  },
+]
+
 function Project() {
   return (
     <div>
+      {projects.map((project) => {
+
+        return (
+          <section class="card text-white m-1">
+            <div class="card-header">{project.title}</div>
+            <figure class="card-body project-image">
+              <a href={project.href} target="_blank" rel="noreferrer">
+                <img src={project.image.src} alt={project.image.alt}></img>
+              </a>
+            </figure>
+          </section>
+        )
+      }
+      )}
+
+      {/* 
       <section class="card text-white m-1">
         <div class="card-header">Horiseon SEO</div>
         <figure class="card-body project-image">
@@ -67,7 +142,7 @@ function Project() {
             <img src={bridal} alt="A gorgeous wedding dress draped in the sun."></img>
           </a>
         </figure>
-      </section>
+      </section> */}
     </div>
   );
 }
