f0625e6d39ec70fe46eac8d208f8c4e28a983e05
Mandell Butler <mandell.butler@outlook.com>
2021-06-16 16:27:25 -0400

    Added Header css styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,51 +5,42 @@ import { useHistory } from 'react-router-dom'
 import headshot from '../assets/images/headshot.png';
 // Here we are importing a CSS file as a dependency
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
+import '../styles/Header.css'
+
 
 // import { palette } from '@material-ui/system';
 
 
-function Display() {
+function Header() {
   const history = useHistory();
   return (
-    <header style={{ width: '100%' }}>
-      <Box display="flex" flexDirection="row" justifyContent="flex-start" p={1}>
-        <Box display="flex" flexDirection="column" alignSelf="flex-start">
-          <figure className="headshot">
-            <img src={headshot} alt="Mandell Posed in Suit Jacket" />
-          </figure>
-        </Box>
-        <Box display="flex" flexDirection="column" alignContent="center" p={1} style={{ width: '100%' }}>
-          <Box display="flex" flexDirection="row" justifyContent="center">
-            <Box display="flex" flexDirection="row" justifyContent="center">
-              <section>
-                <h1>Mandell Butler</h1>
-                <div>
-                  Full Stack Web Developer
-                </div>
-              </section>
-            </Box>
-          </Box>
-          <Box display="flex" flexDirection="column" justifyContent="center">
-            <nav className="navbar">
-              <p><Link onClick={(event) => {
-                history.push('/myWork')
-              }} color='primary'>myWork</Link></p>
-              <p><Link onClick={(event) => {
-                history.push('/aboutMe')
-              }} color='primary'>aboutMe</Link></p>
-              <p><Link onClick={(event) => {
-                history.push('/contact')
-              }} color='primary'>Contact</Link></p>
-              <p><a href="https://github.com/mandellbutler">Github</a></p>
-              <p><a href="https://www.linkedin.com/in/mandellbutler/">Linkedin</a></p>
-            </nav>
-          </Box>
-        </Box>
-      </Box>
-    </header >
+    <div>
+      <header class="container header">
+        <figure className="headshot">
+          <img src={headshot} alt="Mandell Posed in Suit Jacket" />
+        </figure>
+      </header >
+      <section class="namecard">
+        <h1>Mandell Butler</h1>
+        <div>
+          Full Stack Web Developer
+        </div>
+      </section>
+      <nav className="navbar">
+        <p><Link onClick={(event) => {
+          history.push('/myWork')
+        }} color='primary'>myWork</Link></p>
+        <p><Link onClick={(event) => {
+          history.push('/aboutMe')
+        }} color='primary'>aboutMe</Link></p>
+        <p><Link onClick={(event) => {
+          history.push('/contact')
+        }} color='primary'>Contact</Link></p>
+        <p><a href="https://github.com/mandellbutler">Github</a></p>
+        <p><a href="https://www.linkedin.com/in/mandellbutler/">Linkedin</a></p>
+      </nav>
+    </div>
   );
 }
 
-export default Display;
+export default Header;
