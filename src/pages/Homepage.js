d599bf2879aa9b599c7908fee85f388a1c2ca981
Mandell Butler <mandell.butler@outlook.com>
2021-07-27 21:03:35 -0400

    Formatted homepage , adjusted contact/footer

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useHistory } from 'react-router-dom'
 import Button from '../components/Navbar/Button'
+import nyc from "../assets/images/mbgraphic.JPG"
 import '../styles/Homepage.css'
 
 
@@ -11,13 +12,21 @@ function Homepage() {
   const history = useHistory();
 
   return (
-    <div className="homepage-container">
-      <div className="homepage">
-        <Button buttonSize="btn--large" onClick={(event) => {
-          history.push("/aboutMe")
-        }}>
-          Enter
-        </Button>
+    <div className="homepage-main">
+      <div className="homepage-container">
+        <figure className="image-container">
+          <img className="homepage-image" src={nyc} alt="Mandell as a graphic cartoon" />
+        </figure>
+        <div className="feature-container">
+          <div className="homepage-quote">
+            <p>"Websites for the Living" (or Not)</p>
+          </div>
+          <Button buttonSize="btn--large" onClick={(event) => {
+            history.push("/aboutMe")
+          }}>
+            Enter
+          </Button>
+        </div>
       </div>
     </div>
   );
